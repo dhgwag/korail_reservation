687eede4b6838f304b03e9784a8d8f3e1b737c20
rhkrehdgns <rhkrehdgns@gmail.com>
2026-02-13 13:05:42 +0900

    setup.sh 업데이트

--- setup.sh
+++ setup.sh
@@ -7,4 +7,12 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
+# korail2 라이브러리 패치 (NoResultsError 대신 빈 리스트 반환)
+KORAIL2_PATH=$(python -c "import korail2; print(korail2.__file__)")
+if [ -f "$KORAIL2_PATH" ]; then
+    sed -i.bak 's/raise NoResultsError()/return []  # patched/' "$KORAIL2_PATH"
+    rm -f "${KORAIL2_PATH}.bak"
+    echo "korail2 패치 완료!"
+fi
+
 echo "설치 완료!"
